17552a2030dafa2b5278e506a89d49434fb59f74
Nana Yaw Essilfie <nanayawessilfie2005@gmail.com>
2025-06-24 19:47:26 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,286 @@
+import json
+import os
+import random
+
+class Login:
+    def sign_up(self):
+        pass
+    def sign_in(self):
+        pass
+
+class Book_Management:
+    def save_book(self, book):
+        with open('BookList.json', 'w') as file:
+            return json.dump(book, file, indent=4)
+    
+    def load_books(self):
+        if os.path.exists("BookList.json"):
+            with open("BookList.json", "r") as file:
+                content = file.read().strip()
+                if content:
+                    return json.loads(content)
+                else:
+                    return []
+        else:
+            return []
+               
+    def add_book(self):
+        serial_numbers = 0 
+        book = dict()
+        title = input("\nInput title of book: ")
+        author = input("Input author's name: ")
+        serial = random.randint(11111, 99999)
+        while True:
+            price = input("How much will the book be selling for: Ghc ")
+            try:
+                price = float(price)
+                break
+            except ValueError:
+                print('Enter a proper amount')
+                continue
+        input("\nPress enter to continue\n") 
+
+        book['title'] = title
+        book['author'] = author
+        book['serial'] = title[0] + title[-1].upper() + author[0] + str(serial)
+        book['price'] = price
+        book["borrow"] = 'NO'
+        books = self.load_books()
+        books.append(book)
+        self.save_book(books)
+        print("\nBook added successfully\n")
+    
+    def view_all_books(self):
+        books = self.load_books()
+        num = 0
+        for book in books:
+            num += 1
+            print(f"""
+{num}. Book title: {book['title']}
+   Author: {book['author']}
+   Serial Number: {book['serial']}
+   Borrrow Price(per month): GHc {book['price']}
+""")
+        input("\nPress enter to continue\n") 
+
+    def search_by_title(self):
+        search_t = input("\nEnter book title: ")
+        #search_a = input("Enter the author of the book: ")
+        found = False
+        books = self.load_books()
+        num = 0
+        for book in books:
+            if book['title'].lower() == search_t.lower():
+                print(f"""
+Book title: {book['title']}
+Author: {book['author']}
+Serial Number: {book['serial']}
+Borrrow Price(per month): GHc {book['price']}
+""")
+                found = True
+        
+        if not found:
+            print('Book was not found')
+
+        input("\nPress enter to continue\n") 
+
+    def borrow_book(self):
+        bookb = dict()
+        search_t = input("\nEnter title of book you would like to borrow: ")
+    
+        found = False
+        books = self.load_books()
+        num = 0
+        books_found = []
+        for book in books:
+            if book['title'].lower() == search_t.lower():
+                print(f"""
+Book title: {book['title']}
+Author: {book['author']}
+Serial Number: {book['serial']}
+Borrrow Price(per month): GHc {book['price']}
+Borrowed: {book["borrow"]}
+""")
+                books_found.append(book)
+                found = True
+        
+        if found:
+            found = False
+
+            while(True):
+                borrow = input("Enter serial number of book you want to borrow: ")
+                try:
+                    month = int(input("How month would you like to borrow book for(1-24): "))
+                    if month < 1 or month > 24:
+                        print("Select a month within the range given")
+                    else:
+                        break
+
+                except ValueError:
+                    print("Enter a valid number of months")
+
+            for book in books:
+                if borrow.lower() == book['serial'].lower() and book['borrow'] == "NO":
+                    pay = book['price'] * month
+                    print(f"Amount to borrow {book['title']} for {month} month(s) is GHC {pay} ")
+                    bookb['title'] = book['title']
+                    bookb['author'] = book['author']
+                    bookb['serial'] = book['serial']
+                    bookb['price'] = book['price']
+                    bookb["borrow"] = 'YES'
+                    books.remove(book)
+                    while True:
+                        payment  = input("""\nSelect 
+1. To pay online 
+2. To pay at the front desk
+3. To cancel
+Option: """)
+                        if payment in ['1', '2', '3']:
+                            break
+                        else:
+                            print("\nPlease select option available")
+                    if payment == "3":
+                        print("Payment cancelled")
+                        books.append(book)
+                        self.save_book(books)
+                    else:
+                        input("Press enter if done with payment")
+                        print("""Head to library to pick up your book.
+Please note additional charges will be add if book is not return before due date in a readable condition""")
+                        books.append(bookb)
+                        self.save_book(books)
+                    found = True
+                elif borrow.lower() == book['serial'].lower() and book['borrow'] == "YES":
+                    print ('''This book has been borrowed.
+Please select another book in book list that is available for borrowing''')
+                    found = True
+            if not found:
+                print("Book with this serial number was not found")
+
+        else:
+            print('\nBook was not found\n')
+        input("\nPress enter to continue\n") 
+        
+    def display_pending_returns(self):
+        books = self.load_books()
+        num = 0
+        for book in books:
+            if book['borrow'] == 'YES':
+                num += 1
+            
+                print(f"""
+{num}. Book title: {book['title']}
+   Author: {book['author']}
+   Serial Number: {book['serial']}
+   Borrrow Price(per month): GHc {book['price']}
+""")
+        input("\nPress enter to continue\n") 
+    
+    def return_borrowed_book(self):
+        bookb = dict()
+        while True:
+            serial = input("Enter serial number on book: ")
+            found = False
+            books = self.load_books()
+            for book in books:
+                if book['serial'].lower() == serial:
+                    bookb['title'] = book['title']
+                    bookb['author'] = book['author']
+                    bookb['serial'] = book['serial']
+                    bookb['price'] = book['price']
+                    bookb["borrow"] = 'NO'
+                    books.remove(book)
+                    books.append(bookb)
+                    self.save_book(books)
+                    found = True
+                    break
+                elif serial == 0:
+                    break
+
+            if not found:
+                print("Serial number not recognized")
+                print("Please try again or senter 0 to return to main menu" )
+        input("\nPress enter to continue\n") 
+
+    def delete_book(self):
+        serial = input("Enter the serial number of the book to be deleted: ")
+        found = False
+        books = self.load_books()
+        for book in books:
+            if book['serial'] == serial:
+                books.remove(book)
+                self.save_book(books)
+                found = True
+                print (f"Book with serial number: {serial} has been removed from library")
+        
+        if not found:
+            print('Book was not found')
+        input("\nPress enter to continue\n") 
+
+    def sort_book(self):
+        num = 0
+        books = self.load_books()
+        sorted_books = sorted(books, key=lambda x: x['title'])
+        for book in sorted_books:
+            num += 1
+            print(f"""
+{num}. Book title: {book['title']}
+   Author: {book['author']}
+   Serial Number: {book['serial']}
+   Borrrow Price(per month): GHc {book['price']}""")
+    
+    def menu(self):
+        print("""
+    \n--------WELCOME-------
+         TO
+    ESSILFIE LIBRARY\n""")
+        while True:
+            while True:
+                print("""1. Add a book
+2. View all books
+3. Search by title
+4. Borrow book
+5. Display pending returns
+6. Return borrowed book
+7. Delete a book
+8. Sort book in alphabetical order
+9. Exit
+                    """)
+                options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                try:
+                    choice = int(input('What would you like to do: '))
+                    if choice not in options:
+                        print ("\nPlease select a number from the options provided!")
+                    else:
+                        break
+                except ValueError:
+                    print("\nPlease select a number from the provided options!")
+                    continue
+
+            if choice == 1:
+                self.add_book()
+            elif choice == 2:
+                self.view_all_books()
+            elif choice == 3:
+                self.search_by_title()
+            elif choice == 4:
+                self.borrow_book()
+            elif choice == 5:
+                self.display_pending_returns()
+            elif choice == 7:
+                self.delete_book()
+            elif choice == 6:
+                self.return_borrowed_book()
+            elif choice == 8:
+                self.sort_book()
+            else:
+                print("Goodbye.....")
+                break
+            
+
+
+if __name__ == "__main__":
+    library = Book_Management()
+    library.menu()
+
+    
