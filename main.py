18405f935eb2f77e3d13a22f3f3b171adcbe860d
Nana Yaw Essilfie <nanayawessilfie2005@gmail.com>
2025-08-02 08:59:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+# main.py
 import json
 import os
 import random
+import sys
+import datetime
+from login import Login  
 
-class Login:
-    def sign_up(self):
-        pass
-    def sign_in(self):
-        pass
+
 
 class Book_Management:
+    def __init__(self):
+        self.current_user_email = "Unknown"
+
+    def log_activity(self, user_email, action):
+        with open("activity_log.txt", "a") as log:
+            log.write(f"{datetime.datetime.now()} | {user_email} | {action}\n")
+
     def save_book(self, book):
         with open('BookList.json', 'w') as file:
             return json.dump(book, file, indent=4)
@@ -27,8 +34,12 @@ class Book_Management:
     def add_book(self):
         serial_numbers = 0 
         book = dict()
-        title = input("\nInput title of book: ")
-        author = input("Input author's name: ")
+        title = input("\nInput title of book: ").capitalize()
+        author = input("Input author's name: ").capitalize()
+        
+        if len(title) < 2 or len(author) < 1:
+            print("Title must be at least 2 characters and author at least 1 character.")
+            return
         serial = random.randint(11111, 99999)
         while True:
             price = input("How much will the book be selling for: Ghc ")
@@ -49,7 +60,8 @@ class Book_Management:
         books.append(book)
         self.save_book(books)
         print("\nBook added successfully\n")
-    
+        self.log_activity(self.current_user_email, f"Added book: {title}")
+
     def view_all_books(self):
         books = self.load_books()
         num = 0
@@ -61,11 +73,11 @@ class Book_Management:
    Serial Number: {book['serial']}
    Borrrow Price(per month): GHc {book['price']}
 """)
+        self.log_activity(self.current_user_email, "Viewed all books")
         input("\nPress enter to continue\n") 
 
     def search_by_title(self):
         search_t = input("\nEnter book title: ")
-        #search_a = input("Enter the author of the book: ")
         found = False
         books = self.load_books()
         num = 0
@@ -81,7 +93,7 @@ Borrrow Price(per month): GHc {book['price']}
         
         if not found:
             print('Book was not found')
-
+        self.log_activity(self.current_user_email, f"Searched for book title: {search_t}")
         input("\nPress enter to continue\n") 
 
     def borrow_book(self):
@@ -110,7 +122,7 @@ Borrowed: {book["borrow"]}
             while(True):
                 borrow = input("Enter serial number of book you want to borrow: ")
                 try:
-                    month = int(input("How month would you like to borrow book for(1-24): "))
+                    month = int(input("How many months would you like to borrow book for (1-24): "))
                     if month < 1 or month > 24:
                         print("Select a month within the range given")
                     else:
@@ -143,12 +155,14 @@ Option: """)
                         print("Payment cancelled")
                         books.append(book)
                         self.save_book(books)
+                        self.log_activity(self.current_user_email, f"Cancelled borrowing book: {book['title']}")
                     else:
                         input("Press enter if done with payment")
                         print("""Head to library to pick up your book.
-Please note additional charges will be add if book is not return before due date in a readable condition""")
+Please note additional charges will be added if book is not returned before due date in a readable condition""")
                         books.append(bookb)
                         self.save_book(books)
+                        self.log_activity(self.current_user_email, f"Borrowed book: {book['title']} for {month} month(s)")
                     found = True
                 elif borrow.lower() == book['serial'].lower() and book['borrow'] == "YES":
                     print ('''This book has been borrowed.
@@ -174,16 +188,19 @@ Please select another book in book list that is available for borrowing''')
    Serial Number: {book['serial']}
    Borrrow Price(per month): GHc {book['price']}
 """)
+        self.log_activity(self.current_user_email, "Viewed pending returns")
         input("\nPress enter to continue\n") 
     
     def return_borrowed_book(self):
         bookb = dict()
         while True:
-            serial = input("Enter serial number on book: ")
+            serial = input("Enter serial number on book (or 0 to return to main menu): ")
+            if serial == "0":
+                break
             found = False
             books = self.load_books()
             for book in books:
-                if book['serial'].lower() == serial:
+                if book['serial'].lower() == serial.lower():
                     bookb['title'] = book['title']
                     bookb['author'] = book['author']
                     bookb['serial'] = book['serial']
@@ -193,13 +210,14 @@ Please select another book in book list that is available for borrowing''')
                     books.append(bookb)
                     self.save_book(books)
                     found = True
+                    print("Book returned successfully.")
+                    self.log_activity(self.current_user_email, f"Returned book: {book['title']}")
                     break
-                elif serial == 0:
-                    break
-
-            if not found:
+            if found:
+                break
+            else:
                 print("Serial number not recognized")
-                print("Please try again or senter 0 to return to main menu" )
+                print("Please try again or enter 0 to return to main menu")
         input("\nPress enter to continue\n") 
 
     def delete_book(self):
@@ -212,6 +230,7 @@ Please select another book in book list that is available for borrowing''')
                 self.save_book(books)
                 found = True
                 print (f"Book with serial number: {serial} has been removed from library")
+                self.log_activity(self.current_user_email, f"Deleted book: {book['title']}")
         
         if not found:
             print('Book was not found')
@@ -228,8 +247,27 @@ Please select another book in book list that is available for borrowing''')
    Author: {book['author']}
    Serial Number: {book['serial']}
    Borrrow Price(per month): GHc {book['price']}""")
+        self.log_activity(self.current_user_email, "Sorted books alphabetically")
     
-    def menu(self):
+    def Welcome(self):
+        print("""
+    \n--------WELCOME-------
+         TO
+    ESSILFIE LIBRARY\n""")
+    def Login(self):
+        print("""1. Sign In
+2. Sign up
+""")    
+        while True:
+            option = input("Select an option to get access to the library: ").strip()
+            if option == "1":
+                Login().sign_in()
+            elif option == "2":
+                Login().sign_up()
+            else:
+                print("Invalid input, Try again")
+
+    def adminMenu(self):
         print("""
     \n--------WELCOME-------
          TO
@@ -274,13 +312,54 @@ Please select another book in book list that is available for borrowing''')
             elif choice == 8:
                 self.sort_book()
             else:
+                self.log_activity(self.current_user_email, "Logged out (admin)")
                 print("Goodbye.....")
-                break
+                sys.exit()
+                
+    def userMenu(self):
+        print("""
+    \n--------WELCOME-------
+         TO
+    ESSILFIE LIBRARY\n""")
+        while True:
+            while True:
+                print("""
+1. View all books
+2. Search by title
+3. Borrow book
+4. Return borrowed book
+5. Sort book in alphabetical order
+6. Exit
+                    """)
+                options = [1, 2, 3, 4, 5, 6] 
+                try:
+                    choice = int(input('What would you like to do: '))
+                    if choice not in options:
+                        print ("\nPlease select a number from the options provided!")
+                    else:
+                        break
+                except ValueError:
+                    print("\nPlease select a number from the provided options!")
+                    continue
+
+            
+            if choice == 1:
+                self.view_all_books()
+            elif choice == 2:
+                self.search_by_title()
+            elif choice == 3:
+                self.borrow_book()
+            elif choice == 4:
+                self.return_borrowed_book()
+            elif choice == 5:
+                self.sort_book()
+            else:
+                self.log_activity(self.current_user_email, "Logged out (user)")
+                print("Goodbye.....")
+                sys.exit()
             
 
-
 if __name__ == "__main__":
-    library = Book_Management()
-    library.menu()
-
+    Book_Management().Welcome()
+    Book_Management().Login()
     
